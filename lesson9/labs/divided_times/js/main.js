f25cbc915330935c656c49f30f26149d774c856d
orcsab <orcsab@gmail.com>
2014-05-13 21:46:32 +0800

    done!

--- lesson9/labs/divided_times/js/main.js
+++ lesson9/labs/divided_times/js/main.js
@@ -1,55 +1,63 @@
 $(function() {
-  var drops = ['international', 'politics', 'business', 'technology',
-               'culture', 'blogs'];
+  //  setup a click handler for each of the nav options.
+  $('nav').on('click','li', function(event) {showDropDown(event, $(this).attr('id'));} );
 
-  $('.international').click( function () {showDropDown ('international'); } );
-  $('.politics').click( function () {showDropDown ('politics'); } );
-  $('.business').click( function () {showDropDown ('business'); } );
-  $('.technology').click( function () {showDropDown ('technology'); } );
-  $('.culture').click( function () {showDropDown ('culture'); } );
-  $('.blogs').click( function () {showDropDown ('blogs'); } );
-
-  function showDropDown (name) {
+  //  show the named drop down.
+  //  arguments:
+  //  *  event: the event passed to click callback
+  //  *  name: the name of the selected menu item.  this name matches an entry in the
+  //           drops variable.
+  function showDropDown (event, name) {
+    console.log('showDropDown called with name = ' + name);
     var id = '#' + name + '-drop';
     //  special case: if the named dropdown is already being displayed then hide
     //  the drop down menu.
     if ($(id).hasClass('open')) {
-      $('#slide-down').removeClass('open');
-      $('#slide-down').hide();
+      $slidedown = $('#slide-down');
+      $slidedown.slideUp().removeClass('open');
       $(id).removeClass('open');
       $('#primary-nav').children().removeClass('active');
     }
     else {
-      //  make sure slide-down is open and showed.  these statements may be redundant
-      //  as previous calls may have left the menu open.
-      $('#slide-down').addClass('open');
-      $('#slide-down').show();
-
-      //  Hide every drop-down.  This is overkill since only one of them can currently
-      //  be shown.
-      for (i in drops) {
-        var loopId = '#' + drops[i] + '-drop';
-        $(loopId).hide();
-        $(loopId).removeClass('open');
+      //  first check if there are no active members of the nav menu.  if there
+      //  are none, then the nav status is inactive.  if it is active, then logic
+      //  below will avoid some things that are redundant or unattractive to
+      //  a visible menu.
+      var navActive = false;
+      if ($('#primary-nav').children('.active').length !== 0) {
+        navActive = true;
       }
 
-      //  Show the selected submenu.
-      $(id).addClass('open');
-      $(id).show();
+      //  Hide everything.  Of course this is overkill because everything but
+      //  one is already hidden.  But this is a single-line of code so I like.
+      $('#slide-down').children().hide().removeClass('open');
 
-      //  Make sure the nav bar shows the right selection.
-      $('#primary-nav').children().removeClass('active');
-      $('#primary-nav').children('.' + name).addClass('active');
+      //  Make sure the nav bar shows the right selection.  First clear all selections
+      //  then set the current one to active.
+      $primarynav = $('#primary-nav');
+      $primarynav.children().removeClass('active');
+      $primarynav.children('#' + name).addClass('active');
+
+      //  Show the selected submenu.
+      $(id).addClass('open').show();
+
+      //  show the slide-down ID if its not already shown.
+      if (!navActive)
+        $('#slide-down').addClass('open').slideDown();
     }
 
-    return false;
+    //  stop the scrolling to the newly revealed submenu.  it seems either of the
+    //  two statements does this.  which should I use?
+    event.preventDefault();
   }
 
+  //  not used.
   function display (name) {
     console.log('showing ' + name);
     $(name).show();
   };
 
+  // not used.
   function toggleView (name) {
     console.log('toggling visibility for ' + name);
     $(name).toggle();
